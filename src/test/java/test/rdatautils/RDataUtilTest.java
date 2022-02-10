15ef1600daf9b9a4352b2d58f3a5dd023d910286
per <per@alipsa.se>
2022-02-10 23:02:11 +0100

    add write test

--- src/test/java/test/rdatautils/RDataUtilTest.java
+++ src/test/java/test/rdatautils/RDataUtilTest.java
@@ -1,16 +1,20 @@
 package test.rdatautils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
-import org.renjin.sexp.SEXP;
+import org.renjin.primitives.sequence.IntSequence;
+import org.renjin.sexp.*;
 import se.alipsa.rdatautils.RDataUtil;
+import se.alipsa.renjin.client.datautils.DataType;
 import se.alipsa.renjin.client.datautils.Table;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RDataUtilTest {
 
@@ -27,5 +31,43 @@ public class RDataUtilTest {
       assertEquals(2, table.getValueAsInteger(31,10), "Last column in last row");
       assertEquals(3.215, table.getColumnForName("wt").get(3), "4:th row of wt column");
     }
+  }
+
+  @Test
+  public void testExport() throws IOException {
+    ListVector.NamedBuilder df = new ListVector.NamedBuilder();
+    df.add("names", new StringArrayVector("Kurt", "Susan", "Zoe"));
+    df.add("income", new IntArrayVector(120000, 220202, 303030));
+    df.setAttribute("row.names", new IntSequence(1, 1, 3));
+    df.setAttribute("class", StringVector.valueOf("data.frame"));
+    SEXP sexp = df.build();
+    File outFile = new File("employeeSalaries.rds");
+    RDataUtil.write(sexp, outFile);
+
+    assertTrue(outFile.exists());
+    assertTrue(outFile.length() > 100);
+
+    // Same thing from a table
+    List<String> headerList = new ArrayList<String>() {{
+      add("names");
+      add("income");
+    }};
+    List<List<Object>> rowList = new ArrayList<>();
+    rowList.add(new ArrayList<Object>() {{ add("Kurt"); add(120000); }});
+    rowList.add(new ArrayList<Object>() {{ add("Susan"); add(220202); }});
+    rowList.add(new ArrayList<Object>() {{ add("Zoe"); add(303030); }});
+    List<DataType> dataTypes = new ArrayList<DataType>() {{
+      add(DataType.STRING);
+      add(DataType.INTEGER);
+    }};
+
+    Table table = new Table(headerList, rowList, dataTypes);
+    File outFile2 = new File("employeeSalaries2.rds");
+    RDataUtil.write(table.asDataframe(), outFile2);
+
+    assertTrue(outFile2.exists());
+    assertTrue(outFile2.length() > 100);
+
+    assertEquals(outFile.length(), outFile2.length(), "Length should be equal");
   }
 }
